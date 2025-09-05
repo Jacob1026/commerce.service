e55621cd7eec4818ec038e6f61676faf6e2e36ae
DESKTOP-LKQR1CG\user <vdvgvdwvgy@gmail.com>
2025-09-05 14:27:06 +0800

    複合主鍵

---
+++ src/main/java/com/gtelant/commerce/service/configs/SpringDocConfig.java
@@ -0,0 +1,9 @@
+//package com.gtelant.commerce.service.configs;
+//
+//
+//import jakarta.websocket.OnOpen;
+
+//@OnOpen
+//@Configuration
+//public class SpringDocConfig {
+//}
--- src/main/java/com/gtelant/commerce/service/models/UserSegment.java
+++ src/main/java/com/gtelant/commerce/service/models/UserSegment.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Entity
@@ -33,3 +32,31 @@ public class UserSegment {
     @Column(name ="deleted_at")
     private LocalDateTime deletedAt;
 }
+/*
+
+複合主鍵寫法
+@Entity
+@Table(name = "user_segments")
+@IDClass(UserSegment.class) //指定複合主鍵類別
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserSegment {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "segment_id")
+    private Segment segment;
+
+    @Column(name ="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name ="deleted_at")
+    private LocalDateTime deletedAt;
+}
+*/
