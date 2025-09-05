6aac807d83f56a11056393f57534ea4b22fd2670
DESKTOP-LKQR1CG\user <vdvgvdwvgy@gmail.com>
2025-09-05 15:30:17 +0800

    repositories

--- src/main/java/com/gtelant/commerce/service/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gtelant.commerce.service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class UserRepository extends JpaRepository <User, Integer> {
-}
